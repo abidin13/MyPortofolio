14f0a9f47585ee16c027c1e51ee0d715ebf0e9c9
febriyant abidin <febriyantabidin@gmail.com>
2014-11-05 17:26:45 +0700

    Create save.php

---
+++ save.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST['user_email'])){
+
+include('config.php');
+
+$email = strip_tags($_POST['user_email']);
+
+$sql = 'INSERT INTO subscribers (email) VALUES( "'.$email.'")';
+mysql_query($sql);
+
+echo $email;
+} else { echo '0'; }
+?>
